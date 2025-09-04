21879bd08112dea3d26573bb6e50bb90d14ab633
JAKARIA8785 <ocjakaria8785@gmail.com>
2025-09-04 23:09:01 +0600

    server

--- hallhub-backend/server.js
+++ hallhub-backend/server.js
@@ -306,7 +306,7 @@ app.get('/api/student-lostitems/:studentId', async (req, res) => {
         li.Student_ID,
         si.Name as student_name,
         li.Item_ID,
-        li.Item_Type,
+        i.Item_Type,
         li.Description,
         li.Lost_Time
       FROM lost_item li
