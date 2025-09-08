e58497b9bad4a7f3fcb3502b72b8017adf15eb6b
smshakib <skmdshakib00@gmail.com>
2025-09-08 11:37:35 +0600

    Added VIEW & GROUP BY for Lost Items Summary

--- SQL/VIEW.sql
+++ SQL/VIEW.sql
@@ -11,3 +11,12 @@ FROM complaint c
 JOIN Student_Info s ON c.student_id = s.student_id;
 
 SELECT * FROM defaultdb.ComplaintsView;
+
+CREATE VIEW lost_items_summary AS
+SELECT i.item_type, COUNT(*) AS total_lost
+FROM lost_item li
+JOIN item i ON li.Item_ID = i.Item_ID
+GROUP BY i.item_type
+ORDER BY total_lost DESC;
+
+SELECT * FROM defaultdb.lost_items_summary;
